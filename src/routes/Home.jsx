ae950bf486b1b41b141363ba548821a1f8b81154
CEL-Digital <celsolucoesadm@gmail.com>
2026-08-11 17:11:14 -0300

    fix: padroniza imports e pasta para Sections maiusculo

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,7 @@
-import { ContactSection } from "../components/sections/home/Contato.jsx";
-import { HeroHome } from "../components/sections/home/HeroHome.jsx";
-import { AlertSection } from "../components/sections/home/Interesse.jsx";
-import { AboutSection } from "../components/sections/home/Sobre.jsx";
+import { ContactSection } from "../components/Sections/home/Contato.jsx";
+import { HeroHome } from "../components/Sections/home/HeroHome.jsx";
+import { AlertSection } from "../components/Sections/home/Interesse.jsx";
+import { AboutSection } from "../components/Sections/home/Sobre.jsx";
 
 export function Home() {
     return (
